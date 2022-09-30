ba18d1c0d801438d4a7625e2b033add90a8d421a
MKamui <marioelkamui@gmail.com>
2022-09-29 21:48:14 -0500

    new deploy

---
+++ .next/static/webpack/pages/index.60c2e13e2caf63d1.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./public/assets/projects/snoopy.jpg":
+/*!*******************************************!*\
+  !*** ./public/assets/projects/snoopy.jpg ***!
+  \*******************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/snoopy.d558d068.jpg\",\"height\":935,\"width\":1912,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsnoopy.d558d068.jpg&w=8&q=70\"});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wdWJsaWMvYXNzZXRzL3Byb2plY3RzL3Nub29weS5qcGcuanMiLCJtYXBwaW5ncyI6IjtBQUFBLCtEQUFlLENBQUMsb0tBQW9LIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8uL3B1YmxpYy9hc3NldHMvcHJvamVjdHMvc25vb3B5LmpwZz81NzdkIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IHtcInNyY1wiOlwiL19uZXh0L3N0YXRpYy9tZWRpYS9zbm9vcHkuZDU1OGQwNjguanBnXCIsXCJoZWlnaHRcIjo5MzUsXCJ3aWR0aFwiOjE5MTIsXCJibHVyRGF0YVVSTFwiOlwiL19uZXh0L2ltYWdlP3VybD0lMkZfbmV4dCUyRnN0YXRpYyUyRm1lZGlhJTJGc25vb3B5LmQ1NThkMDY4LmpwZyZ3PTgmcT03MFwifTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./public/assets/projects/snoopy.jpg\n");
+
+/***/ })
+
+});
